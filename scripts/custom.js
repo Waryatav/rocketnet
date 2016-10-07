cee5b4275f9dada6627b55166e11efe397eadb46
Anton <waryatav@gmail.com>
2016-10-07 14:34:11 +0300

    Copy link info

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 	// copy to clipboard
 	console.log($('.copy').length);
-	$('.copy').on('click', function() {
+	//$('.copy').on('click', function() {
+	$(document).on('click','.copy',function(){
 		console.log(this);
 		var link = $(this).siblings('a').attr('href');
 		console.log(link);
@@ -13,12 +14,17 @@ $(document).ready(function() {
 		}).text(link).appendTo('body');
 		$textarea.select();
 		try {
-		    var successful = document.execCommand('copy');
+			var successful = document.execCommand('copy');
 		    var msg = successful ? 'ссылка скопирована в буфер обмена' : 'не удалось скопировать ссылку';
 		    $('.copy2clipboard-msg').text(msg).show('slow');
-			setTimeout(function(){ $('.copy2clipboard-msg').hide('slow'); }, 2000);
+			setTimeout(function(){ $('.copy2clipboard-msg').hide('slow'); }, 4000);
 		} catch (err) {
 		   console.log('Oops, unable to copy');
 		}
 	});
+
+//
+//  	$(document).on('click','.copy-link',function(){
+// 		.console.log($(this).next('.copy2clipboard-msg'));
+//  	});
 });
